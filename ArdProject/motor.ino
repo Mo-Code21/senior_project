beb01c5b5eddfd7a7c58837e99df11d626360c47
Mo-Code21 <mohdmake58@gmail.com>
2021-05-03 04:48:26 -0400

    Adding the arduino code

---
+++ ArdProject/motor.ino
@@ -0,0 +1,28 @@
+
+
+void motor() {
+  // Activate motors
+  // motor 1
+  //digitalWrite(slp1Pin, HIGH);
+  //digitalWrite(rst1Pin, HIGH);
+  // motor 2
+  //digitalWrite(slp2Pin, HIGH);
+  //digitalWrite(rst2Pin, HIGH);
+    
+  // Set the spinning direction clockwise:
+  // motor 1
+  digitalWrite(dir1Pin, HIGH);
+  // motor 2
+  digitalWrite(dir2Pin, HIGH);
+
+  // Spin the stepper motor 10 BOA steps
+  for (int i = 0; i < 57; i++) {
+    digitalWrite(step1Pin, HIGH);
+    digitalWrite(step2Pin, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(step1Pin, LOW);
+    digitalWrite(step2Pin, LOW);
+    delayMicroseconds(1000);
+  }
+  delay(500);
+}
