f93fbd60062d144fed2353c0c211d60d2c740ee4
Mo-Code21 <mohdmake58@gmail.com>
2021-05-06 21:40:56 -0400

    started on the senior script

--- Senior.py
+++ Senior.py
@@ -3,24 +3,45 @@
 # Processing sensor data and controlling motor
 
 # Importing Libraries
+import os
 import serial
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+import tkinter as tk
+from sensor import *
+from motor import *
 
-
-arduino = serial.Serial(port='COM3', baudrate=57600, timeout=.1)
+# Establish Arduino serial connection
+arduino = serial.Serial(port='COM3', baudrate=57600, timeout=10)
 print('Established serial connection to Arduino')
 
+# read sensor data
+# row_data = read_sensor()
+# print(row_data)
+# time.sleep(1)
 
-def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
-    time.sleep(0.05)
-    data = arduino.readline()
-    return data
+# convert the row data to voltage
+# voltage = get_voltage(row_data)
+
+# plotting the voltage data
+# plt.style.use('fivethirtyeight')
+# plt.plot([], [])
+# ani = FuncAnimation(plt.gcf(), animate, interval=100)
+# plt.tight_layout()
+# plt.show()
+
+# save_data(voltage_data, voltage)
 
 
-while True:
-    num = input("Enter a number: ")  # Taking input from user
-    value = write_read(num)
-    print(value)  # printing the value
+# def write_read(x):
+#     arduino.write(bytes(x, 'utf-8'))
+#     time.sleep(0.05)
+#     data = arduino.readline()
+#     return data
+
+
+# while True:
+#     num = input("Enter a number: ")  # Taking input from user
+#     value = write_read(num)
+#     print(value)  # printing the value
--- sensor.py
+++ sensor.py
@@ -3,32 +3,53 @@
 # sensor code
 
 
-# Importing Libraries
-import serial
-import time
-import csv
-arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
-
-
-def write_read():
+def read_sensor():
     # arduino.write(bytes(x, 'utf-8'))
     # time.sleep(0.05)
     raw_data = arduino.readline()
     data = str(arduino_data.decode("utf-8"))
+    print("data Recoded")
 
     return data
 
 
-def save_data(data):
+def get_voltage(data):
+    A = 128*((2**24)-1)
+    print("Pressure calculated")
+    return str(5*(data/A))
+
+
+def animate(data):
+    x = range(0, length(data)-1)
+    y = data
+
+    ax = plt.gca()
+    line = ax.lines
+
+    line.set_data(x, y)
+
+    xlim_low, xlim_high = ax.get_xlim()
+    ylim_low, ylim_high = ax.get_ylim()
+
+    ax.set_xlim(xlim_low, (x.max() + 5))
+
+    ymax = y.max()
+
+    ymin = y.min()
+
+    ax.set_ylim((ymin - 5), (ymax + 5))
+
+
+def save_data(filename, data):
     # filename = time.strftime("%Y-%m-%d_%H%M")
     if not os.path.isfile("data.csv"):
         print("hi")
-        with open("data.csv", 'w') as file:
+        with open(filename+".csv", 'w') as file:
             file.write("time,")
             file.write("readings,")
             file.write('\n')
 
-    with open("data.csv", 'a') as file:
+    with open(filename+".csv", 'a') as file:
         file.write(str(data[0])+",")
         file.write(str(data[1])+",")
         file.write('\n')
