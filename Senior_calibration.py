348d07bcc4b66f2da639d8cc84cef89b4caf305d
Mo-Code21 <mohdmake58@gmail.com>
2021-05-13 16:37:13 -0400

    This code is made for calibration. It has the capability to contol the motor and collect sensor data at the same time.

--- Senior_calibration.py
+++ Senior_calibration.py
@@ -6,12 +6,11 @@
 import os
 import serial
 import time
+import csv
 import matplotlib.pyplot as plt
 import numpy as np
 import tkinter as tk
 from matplotlib.animation import FuncAnimation
-from sensor import *
-from motor import *
 
 
 # def animate(i):
@@ -40,45 +39,66 @@ from motor import *
 
 #     ax.set_ylim((ymin - 5), (ymax + 5))
 
+def motor():
+    window = tk.Tk()
+    window.configure(background="white")
+    window.geometry("220x80")
+    window.title("Motor Control")
 
-arduino.close()
+    def collect_data():
+        # read sensor data
+        raw_data = arduino.readline()
+        data = str(raw_data.decode('utf-8'))
+        print(data)
+        time.sleep(0.5)
+
+        # saving reding to file
+        with open("data.csv", 'a') as file:
+            file_writer = csv.writer(file, delimiter=',')
+            file_writer.writerow([str(data)])
+            print("data printed")
+
+        window.after(500, collect_data)
+
+    def run():
+        arduino.write(b'r')
+        print('run')
+
+    def close():
+        arduino.write(b'c')
+        print('close')
+        arduino.close()
+        window.destroy()
+
+    b1 = tk.Button(window, text="Run", command=run)
+
+    b2 = tk.Button(window, text="Close", command=close)
+
+    b1.grid(row=1, column=0)
+
+    b2.grid(row=1, column=2)
+
+    window.after(500, collect_data)
+
+    window.mainloop()
+
+
+if 'arduino' in globals():
+    arduino.close()
 # Establish Arduino serial connection
 arduino = serial.Serial(port='COM3', baudrate=57600,)
 print('Established serial connection to Arduino')
 
 t = 0  # initiate time variable
-while True:
-
-    # read sensor data
-    raw_data = arduino.readline()
-    data = int(raw_data.decode("utf-8"))
-    print(data)
-    t += 1
-    time.sleep(0.5)
-
-    # saving reding to file
-    with open("data.csv", 'a') as file:
-        file.write(str(t)+",")
-        file.write(str(data)+",")
-        file.write('\n')
-        print("data printed")
-
-    # plotting the voltage data
-    # plt.style.use('fivethirtyeight')
-    # plt.plot([], [])
-    # ani = FuncAnimation(plt.gcf(), animate, fargs=(t, data), interval=500)
-    # plt.tight_layout()
-    # plt.show()
-
-
-# def write_read(x):
-#     arduino.write(bytes(x, 'utf-8'))
-#     time.sleep(0.05)
-#     data = arduino.readline()
-#     return data
-
-
 # while True:
-#     num = input("Enter a number: ")  # Taking input from user
-#     value = write_read(num)
-#     print(value)  # printing the value
+motor()
+
+# x = input("run(r) or not(any):")
+# if x == 'r'r:
+#     arduino.write(bytes(x, 'utf-8'))
+# plotting the voltage data
+# plt.style.use('fivethirtyeight')
+# plt.plot([], [])
+# ani = FuncAnimation(plt.gcf(), animate, fargs=(t, data), interval=500)
+# plt.tight_layout()
+# plt.show()
